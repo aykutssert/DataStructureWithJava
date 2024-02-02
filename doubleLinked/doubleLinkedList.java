adf8e096b7bc3bc36e1c1308a773c731a54d46d8
Aykut Sert <aykutssert@gmail.com>
2024-02-03 00:30:45 +0300

    stack added with generic structure

--- doubleLinked/doubleLinkedList.java
+++ doubleLinked/doubleLinkedList.java
@@ -1,3 +1,4 @@
+package doubleLinked;
 
 public class doubleLinkedList {
 
--- stack/Main.java
+++ stack/Main.java
@@ -2,48 +2,57 @@ package stack;
 
 public class Main {
     public static void main(String[] args){
-        Stack myStack = new Stack(2);
-        myStack.Push(3);
-        myStack.Push(4);
-        myStack.Push(5);
-        myStack.Push(6);
-        myStack.Push(7);
-        myStack.Push(8);
-        myStack.Push(9);
-    
+        Generic g1 = new Generic("Aykut");
+        Generic g2 = new Generic("Sert");
+        Generic g3 = new Generic("Tukya");
+        Generic g4 = new Generic("Tres");
+        Stack<Generic> myStack = new Stack<>(g1);
+        myStack.Push(g2);
+        myStack.Push(g3);
+        myStack.Push(g4);
         myStack.print();
-        System.out.println("Pop data:"+myStack.Pop());
+        
         
         
     }
-    public static class Stack{
+    public static class Generic{
+        public String name;
+        public Generic(String name) {
+            this.name = name;
+        }
+        @Override 
+        public String toString() {
+            return "Name:"+name;
+        }
+    }
+    public static class Stack<T>{
         
-        public Node front;
-        public Node rear;
-        public Node iter;
-        public Stack(int data) {
-            Node node = new Node(data);
+        public Node<T> front;
+        public Node<T> rear;
+        public Node<T> iter;
+        public Stack(T data) {
+            Node<T> node = new Node<T>(data);
             front = node;
             iter = node;
             rear = node;
             front.next=null;
         }
-        public void Push(int data){
-            Node node = new Node(data);
-            Node iterPrev = iter;
+        public void Push(T data){
+            Node<T> node = new Node<T>(data);
+            Node<T> iterPrev = iter;
             iter = node;
             iter.next = iterPrev;
             rear = node;
             
 
         }
-        public int Pop(){
-            int num = rear.data;
+        public T Pop(){
+            T num = rear.data;
             rear = rear.next;
             return num;
         }
         public void print() {
-            Node temp = rear;
+            Node<T> temp = rear;
             while (temp != null) {
                 System.out.print(temp.data+"-");
         
@@ -52,10 +61,10 @@ public class Main {
             System.out.println();  // Add a new line after printing the list
         }
     }
-    public static class Node {
-        public int data;
-        public Node next;
-        public Node(int data) {
+    public static class Node <T>{
+        public T data;
+        public Node<T> next;
+        public Node(T data) {
             this.data = data;
             next = null;
         }
